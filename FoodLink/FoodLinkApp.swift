2c89de6a41f5cffcbbbfa3560cdbd7097fce555f
Nischal Niroula <officialnischalniroula@gmail.com>
2024-05-11 20:37:44 +1000

    Initial Commit

---
+++ FoodLink/FoodLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodLinkApp.swift
+//  FoodLink
+//
+//  Created by Nischal Niroula on 11/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
